2a132f5a286fa68bcc9e098657f2dfdbe976cfb1
unknown <man@vlad.prolisok.org>
2014-02-11 13:59:35 +0200

    some changes

--- emperor_vm/build/Debug/MinGW-Windows/interpreter.o.d
+++ emperor_vm/build/Debug/MinGW-Windows/interpreter.o.d
@@ -1,8 +1,10 @@
 build/Debug/MinGW-Windows/interpreter.o: interpreter.c virtual_machine.h \
- common.h interpreter.h
+ common.h memory_manager.h interpreter.h
 
 virtual_machine.h:
 
 common.h:
+
+memory_manager.h:
 
 interpreter.h:

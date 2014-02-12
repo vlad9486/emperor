2bb2ed42e288e94668d146848bf4c0562c7ef326
unknown <man@vlad.prolisok.org>
2014-02-12 11:58:59 +0200

    purge

--- emperor_vm/build/Debug/MinGW-Windows/main.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/MinGW-Windows/main.o: main.c virtual_machine.h common.h
-
-virtual_machine.h:
-
-common.h:
--- emperor_vm/build/Debug/MinGW-Windows/memory_manager.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/MinGW-Windows/memory_manager.o: memory_manager.c \
- memory_manager.h common.h
-
-memory_manager.h:
-
-common.h:
